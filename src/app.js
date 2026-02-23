1d28c3f268bf0562deee2b0be9b1b33d9c5f17aa
ANKIT TIWARY <techyjantadoyo121@gmail.com>
2026-02-23 21:12:24 +0530

    routes changed for deployment

--- src/app.js
+++ src/app.js
@@ -24,18 +24,18 @@ const {userRouter} = require("./routes/userRoutes");
 const {reviewRouter} =require("./routes/reviewRoutes");
 
 
-app.get("/api",async(req,res)=>{
+app.get("/",async(req,res)=>{
     res.send("Server is setup")
 });
 
 
 //authRouter
-app.use("/api",authRouter);
-app.use("/api",requestRouter);
-app.use("/api",userRouter);
-app.use("/api",reviewRouter);
+app.use("/",authRouter);
+app.use("/",requestRouter);
+app.use("/",userRouter);
+app.use("/",reviewRouter);
 //secure api acess
-app.get("/api/profile",userAuth,async(req,res)=>{
+app.get("/profile",userAuth,async(req,res)=>{
     const {name,age,email} =req.user;
     res.send({
         "message":`Welcome ${name}`,
