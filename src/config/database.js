9531a81b8492942991ba316908e9150500624b9a
ANKIT TIWARY <techyjantadoyo121@gmail.com>
2026-03-01 00:28:38 +0530

    changed mongodb connection url

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,6 @@ const DBconnect = ()=>{
     try{
 
          mongoose.connect("mongodb+srv://ent_ankittiwary:%23AnkitDevConnect01@cluster0.oicemgl.mongodb.net/DevConnect?retryWrites=true&w=majority");
-       
     }
     catch(err){
         console.log("Connection to DB failed");
