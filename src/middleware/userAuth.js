937e68dacfee655707a3b906f7125ab800524162
ANKIT TIWARY <techyjantadoyo121@gmail.com>
2026-03-24 16:18:54 +0530

    db URl updated

--- src/middleware/userAuth.js
+++ src/middleware/userAuth.js
@@ -1,6 +1,6 @@
 //token nikal k validate krna hain 
 const jwt = require("jsonwebtoken");
-
+const jwtSecretKey = process.env.JWT_SECRET;
 const userB =require("../model/customer");
 const userAuth = async(req,res,next)=>{
     try{
@@ -11,7 +11,7 @@ const userAuth = async(req,res,next)=>{
         }
         //validate the given token
 
-        const decodedMessageObject =await jwt.verify(token,"<mysecretkey>");
+        const decodedMessageObject =await jwt.verify(token,jwtSecretKey);
         // decoded token will store (_id,pat,expiry)
         const {_id}= decodedMessageObject;
         const user = await userB.findOne({_id:_id});
