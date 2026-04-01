01a877cd3af78a5d035032bc3cc1e6a5761d9aca
ANKIT TIWARY <techyjantadoyo121@gmail.com>
2026-04-02 03:40:23 +0530

    deleted unwanted files

--- src/utils/sesClient.js
+++
@@ -1,9 +0,0 @@
-const { SESClient} = require("@aws-sdk/client-ses");
-const REGION = "ap-south-1";
-const sesClient = new SESClient({region:REGION, 
-    credentials:{
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAcessKey: process.env.AWS_SES_SECRET_KEY
-    }
-});
-module.exports={sesClient};
